a5e8eab6f0cd256d28b70c7280a74aa30d383510
Pug1Gaming <35202715+Pug1Gaming@users.noreply.github.com>
2020-01-05 13:07:40 -0700

    Probably fixed some shit
    Add an optional extended description.

--- potato_mata.mk
+++ potato_mata.mk
@@ -1,10 +1,7 @@
 # Inherit some common potato stuff.
 $(call inherit-product, vendor/potato/config/common_full_phone.mk)
 
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
-$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_n_mr1.mk)
 # Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
